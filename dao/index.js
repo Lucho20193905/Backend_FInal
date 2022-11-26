3925b410908c5ded109a517f7d421fec0d34fd9a
AlonsoCGonzales <alonzotoricaceres@gmail.com>
2022-11-26 03:36:28 -0500

    filtro

--- dao/index.js
+++ dao/index.js
@@ -3,300 +3,300 @@ const { Sequelize, DataTypes } = require("sequelize");
 // postgres://<USUARIO>:<PASSWORD>@<URL_HOST_BD>:<PUERTO_BD>/<NOMBRE_BD>
 
 
-const CADENA_CONEXION = process.env.DATABASE_URL || 
-    "postgresql://evaluaciones:evaluaciones@localhost:5432/postgres"
+const CADENA_CONEXION = process.env.DATABASE_URL ||
+  "postgresql://evaluaciones:evaluaciones@localhost:5432/postgres"
 
-const sequelize = new Sequelize(CADENA_CONEXION,{
-    dialectOptions : {
-        ssl: process.env.DATABASE_URL ? true : false
-    }
+const sequelize = new Sequelize(CADENA_CONEXION, {
+  dialectOptions: {
+    ssl: process.env.DATABASE_URL ? true : false
+  }
 })
 
-    const usuario = sequelize.define("usuario", {
-        id : {
-            primaryKey : true,
-            type : DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        nombre : {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        apellido: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },  
-        correo:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        c_postal: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        telefono: {
-            type : DataTypes.INTEGER,
-            allowNull : true
-        },
-        ciudad: {
-            type :  DataTypes.STRING(200),
-            allowNull : true
-        },
-        direccion: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        contrasenia:{
-            type: DataTypes.STRING(200),
-            allowNull : true
-        }
-    }, {
-        timestamps : false,
-        freezeTableName : true
-    })
-    const producto = sequelize.define("producto",{
-        id : {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        nombre:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        precio: {
-            type : DataTypes.INTEGER,
-            allowNull : true
-        },
-        categoria:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        descripcion: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
+const usuario = sequelize.define("usuario", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  nombre: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  apellido: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  correo: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  c_postal: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  telefono: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  ciudad: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  direccion: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  contrasenia: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  }
+}, {
+  timestamps: false,
+  freezeTableName: true
+})
+const producto = sequelize.define("producto", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  nombre: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  precio: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  categoria: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  descripcion: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
 
-    const orden = sequelize.define("orden",{
-        id : {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        usuario_id:{
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        },
-        monto:{
-            type : DataTypes.INTEGER,
-            allowNull : true
-        },
-        direccion: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        fecha: {
-            type : DataTypes.DATE,
-            allowNull : true
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
-    const orden_producto = sequelize.define("orden_producto",{
-        id : {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        orden_id : {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        },
-        producto_id : {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
-    const pcarmado = sequelize.define("pcarmado",{
-        id : {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        nombre: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        descripcion: {
-            type : DataTypes.STRING(200),
-            allowNull : true
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
-    const pcarmado_producto = sequelize.define("pcarmado_producto",{
-        id: {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        pcarmado_id: {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        },
-        producto_id: {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
-    const reporte =  sequelize.define("reporte",{
-        id: {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        usuario_id: {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        },
-        correo:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        nombre:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        telefono:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        asunto:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        descripcion:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
+const orden = sequelize.define("orden", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  usuario_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  monto: {
+    type: DataTypes.INTEGER,
+    allowNull: true
+  },
+  direccion: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  fecha: {
+    type: DataTypes.DATE,
+    allowNull: true
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
+const orden_producto = sequelize.define("orden_producto", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  orden_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  producto_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
+const pcarmado = sequelize.define("pcarmado", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  nombre: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  descripcion: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
+const pcarmado_producto = sequelize.define("pcarmado_producto", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  pcarmado_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  producto_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
+const reporte = sequelize.define("reporte", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  usuario_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  correo: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  nombre: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  telefono: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  asunto: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  descripcion: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
 
-    const resenia =  sequelize.define("resenia",{
-        id: {
-            primaryKey: true,
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4
-        },
-        usuario_id: {
-            type: DataTypes.UUID,
-            defaultValue : Sequelize.UUIDV4,
-            allowNull : false
-        },
-        puntaje:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        comentario:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        video:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        link:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        },
-        tipo_resenia:{
-            type : DataTypes.STRING(200),
-            allowNull : true
-        }
-    },
-    {
-        timestamps : false,
-        freezeTableName : true
-    })
+const resenia = sequelize.define("resenia", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  usuario_id: {
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4,
+    allowNull: false
+  },
+  puntaje: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  comentario: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  video: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  link: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  },
+  tipo_resenia: {
+    type: DataTypes.STRING(200),
+    allowNull: true
+  }
+},
+  {
+    timestamps: false,
+    freezeTableName: true
+  })
 
-    //Relaciones
-    //reporte *-------->1 usuario
-    reporte.belongsTo(usuario,{
-        foreignKey : "usuario_id"
-    })
-    usuario.hasMany(reporte,{
-        foreignKey : "id"
-    })
-    //resenia *-------->1 usuario
-    resenia.belongsTo(usuario,{
-        foreignKey : "usuario_id"
-    })
-    usuario.hasMany(resenia,{
-        foreignKey : "id"
-    })
-    //orden *-------->1 usuario
-    orden.belongsTo(usuario,{
-        foreignKey : "usuario_id"
-    })
-    usuario.hasMany(orden,{
-        foreignKey : "id"
-    })
-    //orden_producto *-------->1 orden
-    orden_producto.belongsTo(orden,{
-        foreignKey : "orden_id"
-    })
-    orden.hasMany(orden_producto,{
-        foreignKey : "id"
-    })
-    //orden_producto *-------->1 producto
-    orden_producto.belongsTo(producto,{
-        foreignKey : "producto_id"
-    })
-    producto.hasMany(orden_producto,{
-        foreignKey : "id"
-    })
-    //pc_armado_producto *-------->1 producto
-    pcarmado_producto.belongsTo(producto,{
-        foreignKey : "producto_id"
-    })
-    producto.hasMany(pcarmado_producto,{
-        foreignKey : "id"
-    })
-    //pc_armado_producto *-------->1 pc_armado
-    pcarmado_producto.belongsTo(pcarmado,{
-        foreignKey : "pcarmado_id"
-    })
-    pcarmado.hasMany(pcarmado_producto,{
-        foreignKey : "id"
-    })
+//Relaciones
+//reporte *-------->1 usuario
+reporte.belongsTo(usuario, {
+  foreignKey: "usuario_id"
+})
+usuario.hasMany(reporte, {
+  foreignKey: "id"
+})
+//resenia *-------->1 usuario
+resenia.belongsTo(usuario, {
+  foreignKey: "usuario_id"
+})
+usuario.hasMany(resenia, {
+  foreignKey: "id"
+})
+//orden *-------->1 usuario
+orden.belongsTo(usuario, {
+  foreignKey: "usuario_id"
+})
+usuario.hasMany(orden, {
+  foreignKey: "id"
+})
+//orden_producto *-------->1 orden
+orden_producto.belongsTo(orden, {
+  foreignKey: "orden_id"
+})
+orden.hasMany(orden_producto, {
+  foreignKey: "id"
+})
+//orden_producto *-------->1 producto
+orden_producto.belongsTo(producto, {
+  foreignKey: "producto_id"
+})
+producto.hasMany(orden_producto, {
+  foreignKey: "id"
+})
+//pc_armado_producto *-------->1 producto
+pcarmado_producto.belongsTo(producto, {
+  foreignKey: "producto_id"
+})
+producto.hasMany(pcarmado_producto, {
+  foreignKey: "id"
+})
+//pc_armado_producto *-------->1 pc_armado
+pcarmado_producto.belongsTo(pcarmado, {
+  foreignKey: "pcarmado_id"
+})
+pcarmado.hasMany(pcarmado_producto, {
+  foreignKey: "id"
+})
 
-    module.exports = { usuario, producto, orden, orden_producto, pcarmado, pcarmado_producto, reporte, resenia }
+module.exports = { usuario, producto, orden, orden_producto, pcarmado, pcarmado_producto, reporte, resenia }
 /*
 const CADENA_CONEXION = 
     "postgresql://evaluaciones:evaluaciones@localhost:5432/evaluacionesdb"
--- index.js
+++ index.js
@@ -3,115 +3,131 @@ const bodyParser = require("body-parser")
 const cors = require("cors")
 
 const data = require("./test_data") // importamos data de test
-const { usuario, producto , orden , orden_producto, pcarmado, pcarmado_producto, reporte, resenia} = require("./dao")
+const { usuario, producto, orden, orden_producto, pcarmado, pcarmado_producto, reporte, resenia } = require("./dao")
 
 const PUERTO = process.env.PORT || 4444
 
 const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-    extended : true
+  extended: true
 }))
+
 app.use(cors()) // politica CORS (cualquier origen) <---- TODO: cuidado!!!
 app.use(express.static("assets")) // <-- configuracion de contenido estatico
 
+app.post("/usuarios", async (req, resp) => {
+  const dataRequest = req.body
+  const nombre = dataRequest.nombre
+  const apellido = dataRequest.apellido
+  const correo = dataRequest.correo
+  const c_postal = dataRequest.c_postal
+  const telefono = dataRequest.telefono
+  const ciudad = dataRequest.ciudad
+  const direccion = dataRequest.direccion
+  const contrasenia = dataRequest.contrasenia
 
-app.post("/usuarios", async (req,resp) => {
-    const dataRequest = req.body
-    const nombre = dataRequest.nombre
-    const apellido = dataRequest.apellido
-    const correo = dataRequest.correo
-    const c_postal= dataRequest.c_postal
-    const telefono = dataRequest.telefono
-    const ciudad = dataRequest.ciudad
-    const direccion = dataRequest.direccion
-    const contrasenia = dataRequest.contrasenia
+  await usuario.create({
+    nombre: nombre,
+    apellido: apellido,
+    correo: correo,
+    c_postal: c_postal,
+    telefono: telefono,
+    ciudad: ciudad,
+    direccion: direccion,
+    contrasenia: contrasenia,
+  })
 
-    await usuario.create({
-        nombre : nombre,
-        apellido : apellido,
-        correo : correo,
-        c_postal : c_postal,
-        telefono : telefono,
-        ciudad : ciudad,
-        direccion : direccion,
-        contrasenia : contrasenia,
-    })
-
-    resp.send({
-        confirmar: "Registro exitoso"
-    })
-})
-app.post("/reporte", async (req,resp) => {
-    const dataRequest = req.body
-    const correo = dataRequest.correo
-    const nombre = dataRequest.nombre
-    const telefono = dataRequest.telefono
-    const asunto = dataRequest.asunto
-    const descripcion = dataRequest.descripcion
-    //se necesitaria esto? 
-    const usuario_id = dataRequest.usuario_id
-
-    
-    await reporte.create({
-        correo : correo,
-        nombre : nombre,
-        telefono : telefono,
-        asunto : asunto,
-        descripcion : descripcion,
-        usuario_id : usuario_id,
-    })  
-    resp.send({
-        confirmar: "Reporte enviado correctamente"
-    })
-})
-app.post("/resenia", async (req,resp) => {
-    const dataRequest = req.body
-    const puntaje = dataRequest.puntaje
-    const comentario = dataRequest.comentario
-    const video = dataRequest.video
-    const link = dataRequest.link
-    const tipo_resenia = dataRequest.tipo_resenia
-    const usuario_id = dataRequest.usuario_id
-    await resenia.create({
-        puntaje : puntaje,
-        comentario : comentario,
-        video : video,
-        link : link,
-        tipo_resenia : tipo_resenia,
-        usuario_id : usuario_id,
-    })
-    resp.send({
-        confirmar: "Reseña enviada correctamente"
-    })
+  resp.send({
+    confirmar: "Registro exitoso"
+  })
 })
 
+app.post("/reporte", async (req, resp) => {
+  const dataRequest = req.body
+  const correo = dataRequest.correo
+  const nombre = dataRequest.nombre
+  const telefono = dataRequest.telefono
+  const asunto = dataRequest.asunto
+  const descripcion = dataRequest.descripcion
+  //se necesitaria esto? 
+  const usuario_id = dataRequest.usuario_id
+
+
+  await reporte.create({
+    correo: correo,
+    nombre: nombre,
+    telefono: telefono,
+    asunto: asunto,
+    descripcion: descripcion,
+    usuario_id: usuario_id,
+  })
+  resp.send({
+    confirmar: "Reporte enviado correctamente"
+  })
+})
+
+app.post("/resenia", async (req, resp) => {
+  const dataRequest = req.body
+  const puntaje = dataRequest.puntaje
+  const comentario = dataRequest.comentario
+  const video = dataRequest.video
+  const link = dataRequest.link
+  const tipo_resenia = dataRequest.tipo_resenia
+  const usuario_id = dataRequest.usuario_id
+  await resenia.create({
+    puntaje: puntaje,
+    comentario: comentario,
+    video: video,
+    link: link,
+    tipo_resenia: tipo_resenia,
+    usuario_id: usuario_id,
+  })
+  resp.send({
+    confirmar: "Reseña enviada correctamente"
+  })
+})
 
 app.get("/productos", async (req, resp) => {
-    const listaproductos = await producto.findAll()
-    resp.send(listaproductos)
+  const listaproductos = await producto.findAll()
+  resp.send(listaproductos)
 })
+
 app.get("/pcarmado", async (req, resp) => {
+  const desc = req.query.descripcion
+  console.log(desc)
+
+  if (desc == undefined) {
     const listapcarmado = await pcarmado.findAll()
     resp.send(listapcarmado)
+
+  } else {
+    const listapcarmado = await pcarmado.findAll({
+      where: {
+        descripcion: desc
+      }
+    })
+    resp.send(listapcarmado)
+  }
 })
+
 app.get("/pcarm_producto", async (req, resp) => {
-    const listapcarm_produto = await pcarmado_producto.findAll()
-    resp.send(listapcarm_produto)
+  const listapcarm_produto = await pcarmado_producto.findAll()
+  resp.send(listapcarm_produto)
 
 })
 app.get("/historial_compras", async (req, resp) => {
-    const listahistorial_compras = await orden_producto.findAll()
-    resp.send(listahistorial_compras)
+  const listahistorial_compras = await orden_producto.findAll()
+  resp.send(listahistorial_compras)
 })
 app.get("/orden", async (req, resp) => {
-    const listaorden = await orden.findAll()
-    resp.send(listaorden)
-    })
+  const listaorden = await orden.findAll()
+  resp.send(listaorden)
+})
 
 
 app.listen(PUERTO, () => {
-    console.log(`Servidor web iniciado en puerto ${PUERTO}`)
+  console.log(`Servidor web iniciado en puerto ${PUERTO}`)
 })
 /*
 //1. Servicio que nos devuelva una lista de carreras
